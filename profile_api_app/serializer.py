5acbcb8baee1319c761ea7d6ac4585cb46a8e5cd
Your Name <justindo720@gmail.com>
2021-07-26 11:24:28 -0400

    Finished view profile and achievements api

--- profile_api_app/serializer.py
+++ profile_api_app/serializer.py
@@ -1,1 +1,34 @@
 from rest_framework import serializers
+from profile_api_app.models import Profile, Achievement, Student
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+
+
+# NOTE: Use ModelSerializer since we are working with models. If we use serializers.Serializer we just get 200 response
+
+# Profile Serializer takes into account the user's name email and area of interest
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__'
+
+
+# Achievement Serializer records the users achievements
+class AchievementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Achievement
+        fields = "__all__"
+
+
+# Student Serializer records the user's student status like their grades and activities
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = "__all__"
+
+
+# Register Serializer is going to be used to register our users so
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password')
--- profile_api_app/views.py
+++ profile_api_app/views.py
@@ -1,7 +1,67 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Profile, Achievement, Student
+from .serializer import ProfileSerializer, AchievementSerializer, StudentSerializer, RegisterSerializer
 
-# Create your views here.
+
+# Create your APIview here.
+class ShowAllProfile(APIView):
+    """
+    List all the user profile
+        * The user's first and last name
+        * Their email and area of interest
+        * also the time when they joined + their username
+    """
+
+    # Show Profile will only handle get request. When the user registers the signals will take care of profile creation
+    def get(self, request):
+        profiles = Profile.objects.all()
+        serializer = ProfileSerializer(profiles, many=True)
+        return Response(serializer.data)
 
 
-def index(request):
-    return render(request, 'index.html')
+class ShowProfile(APIView):
+    """
+    Shows a specific user profile
+    """
+
+
+class ShowAllAchievements(APIView):
+    """
+    List all the user achievements
+        * achievement response with their username
+    """
+
+    # Show all achievements from everyone
+    def get(self, request):
+        achievements = Achievement.objects.all()
+        serializer = AchievementSerializer(achievements, many=True)
+
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = AchievementSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            # if the serializer is saved then the item is created
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        # if the serializer failed to save then the item is not created
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class ShowAchievements(APIView):
+    """
+    Shows a specific user achievement
+
+    This api will handle post request* Handles posting achievement response
+
+    """
+
+
+
+
+
